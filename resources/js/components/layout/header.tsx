915e89058b198a372c437d8155070567ba7ff07b
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2026-02-04 14:52:53 -0300

    feat: prepare header

---
+++ resources/js/components/layout/header.tsx
@@ -0,0 +1,149 @@
+import { Link, usePage } from '@inertiajs/react';
+import { useCallback, useState } from 'react';
+import { cn } from '@/lib/utils';
+import { home } from '@/routes';
+import PrimaryLink from '../ui/primary-link';
+import { InstagramIcon, LinkedInIcon } from '../logos/logos';
+import { getWidthClasses } from '@/utils/utils';
+import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/solid';
+
+const Header = () => {
+    const { url } = usePage();
+
+    const backgroundColorBasedUrl = useCallback(() => {
+        return url !== home.get().url ? 'bg-transparent border-white border-2' : 'bg-gray-renovacom';
+    }, [url]);
+
+    const navigationItems = [
+        {
+            name: 'Inicio',
+            href: home.get().url,
+            isCurrent: url === home.get().url,
+        },
+        {
+            name: 'Metodología',
+            href: '#methodology',
+            isCurrent: url === '#methodology',
+        },
+        {
+            name: 'Servicios',
+            href: '#services',
+            isCurrent: url === '#services',
+        },
+    ];
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const handleToggleMenu = () => {
+        setToggleMenu(!toggleMenu);
+    };
+
+    return (
+        <header
+            className={cn(
+                backgroundColorBasedUrl(),
+                getWidthClasses(),
+                'my-5 flex items-center justify-between rounded-4xl p-3 lg:rounded-renovacom lg:p-5 lg:py-6',
+            )}
+        >
+            <img
+                src="/assets/logo.png"
+                alt="Renovacom Logo"
+                className="w-64"
+            />
+
+            {/* Header on large screens */}
+            <nav className="hidden lg:block">
+                <ul className="flex items-center gap-8">
+                    {navigationItems.map((item) => (
+                        <li
+                            key={item.name}
+                            className={cn(
+                                'inline-block text-lg font-medium text-white transition-all hover:text-green-renovacom',
+                                item.isCurrent ? 'font-bold' : 'font-normal',
+                            )}
+                        >
+                            <Link href={item.href}>{item.name}</Link>
+                        </li>
+                    ))}
+                    <li>
+                        <Link>
+                            <InstagramIcon className="size-5 fill-white transition-all hover:fill-green-renovacom" />
+                        </Link>
+                    </li>
+                    <li>
+                        <Link>
+                            <LinkedInIcon className="size-5 fill-white transition-all hover:fill-green-renovacom" />
+                        </Link>
+                    </li>
+                    <li>
+                        <PrimaryLink href="#contacto">Agendar una reunión</PrimaryLink>
+                    </li>
+                </ul>
+            </nav>
+
+            {/* Header on small screens */}
+            <div className="lg:hidden">
+                <button
+                    type="button"
+                    role="button"
+                    onClick={handleToggleMenu}
+                    aria-label="Toggle menu"
+                    className="p-2"
+                >
+                    <Bars3Icon className="size-8 text-white" />
+                </button>
+                <div
+                    className={cn(
+                        'fixed top-0 right-0 flex min-h-lvh w-[99%] flex-col gap-10 rounded-l-4xl border-l-2 border-white bg-gray-renovacom p-7 backdrop-blur-xl transition-all',
+                        {
+                            'translate-x-0': toggleMenu,
+                            'translate-x-full': !toggleMenu,
+                        },
+                    )}
+                >
+                    <button
+                        type="button"
+                        role="button"
+                        onClick={handleToggleMenu}
+                        className="ml-auto"
+                        aria-label="Close menu"
+                    >
+                        <XMarkIcon className="size-8 text-white" />
+                    </button>
+                    <nav className="flex flex-col gap-6">
+                        <ul className="flex flex-col gap-6">
+                            {navigationItems.map((item) => (
+                                <li key={item.name}>
+                                    <a
+                                        href={item.href}
+                                        onClick={() => setToggleMenu(false)}
+                                        className={cn('text-xl font-medium text-white hover:underline', item.isCurrent ? 'font-bold' : 'font-normal')}
+                                    >
+                                        {item.name}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                        <div className="border-t border-white pt-6">
+                            <PrimaryLink
+                                href="#contacto"
+                                className="block text-center"
+                            >
+                                Agendar una reunión
+                            </PrimaryLink>
+                        </div>
+                        <div className="flex gap-6 pt-6">
+                            <Link>
+                                <InstagramIcon className="size-7" />
+                            </Link>
+                            <Link>
+                                <LinkedInIcon className="size-6" />
+                            </Link>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+};
+export default Header;
