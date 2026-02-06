c840cc8ccde3bf47c03c92e2b892108b9d4dc2fa
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2026-02-06 10:52:59 -0300

    feat: add decorative SVG lines to Footer component for enhanced visual design

--- resources/js/components/layout/footer.tsx
+++ resources/js/components/layout/footer.tsx
@@ -7,8 +7,75 @@ const Footer = () => {
     const { navigationItems } = useNavigation();
 
     return (
-        <footer className="bg-black-renovacom py-20">
+        <footer className="relative overflow-hidden bg-black-renovacom py-20">
             <div className={cn(getWidthClasses(), 'grid grid-cols-2 gap-14 lg:grid-cols-4 lg:gap-0')}>
+                <svg
+                    className="right-0 hidden min-h-full min-w-full lg:absolute lg:-top-5 lg:-left-76 lg:block lg:scale-[1.1] xl:left-0 xl:scale-[1] 2xl:scale-[1.1]"
+                    width="1440"
+                    height="371"
+                    viewBox="0 0 1440 371"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <line
+                        x1="1440"
+                        y1="81"
+                        y2="81"
+                        stroke="white"
+                        strokeOpacity="0.5"
+                        strokeWidth="2"
+                    />
+                    <line
+                        x1="1440"
+                        y1="327"
+                        y2="327"
+                        stroke="white"
+                        strokeOpacity="0.5"
+                        strokeWidth="2"
+                    />
+                    <line
+                        x1="1402"
+                        y1="430"
+                        x2="1402"
+                        y2="-4.37114e-08"
+                        stroke="white"
+                        strokeOpacity="0.5"
+                        strokeWidth="2"
+                    />
+                    <line
+                        x1="1395"
+                        y1="327"
+                        x2="1409"
+                        y2="327"
+                        stroke="#F8FEFF"
+                        strokeWidth="2"
+                    />
+                    <line
+                        x1="1402"
+                        y1="334"
+                        x2="1402"
+                        y2="320"
+                        stroke="#F8FEFF"
+                        strokeWidth="2"
+                    />
+                    <line
+                        x1="1395"
+                        y1="81"
+                        x2="1409"
+                        y2="81"
+                        stroke="#F8FEFF"
+                        strokeWidth="2"
+                    />
+                    <line
+                        x1="1402"
+                        y1="88"
+                        x2="1402"
+                        y2="74"
+                        stroke="#F8FEFF"
+                        strokeWidth="2"
+                    />
+                </svg>
+
                 <img
                     src="/assets/logo-footer.png"
                     alt="Logo Renovacom"
