f9e9cfa7f9133bb578afceb97f7aa6987828c49a
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2026-02-06 11:54:23 -0300

    feat: add MethodologyController and ServicesController, update routes for new endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MethodologyController;
+use App\Http\Controllers\ServicesController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', HomeController::class)->name('home');
+Route::get('/metodologia', MethodologyController::class)->name('metodologia');
+Route::get('/servicios', ServicesController::class)->name('servicios');
